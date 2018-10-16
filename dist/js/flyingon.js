f6789458a9b9e7c3e7a356a402be226b2eba2610
yaozy <yaozy@126.com>
2018-10-16 21:36:49 +0800

    update

--- dist/js/flyingon.js
+++ dist/js/flyingon.js
@@ -2856,19 +2856,21 @@ Function.prototype.bind || (Function.prototype.bind = function (context) {
 
 
 
+    // 可以指定小数位的四舍五入算法
+    Number.prototype.round2 = function (digits) {
+
+        return new Decimal(this).round(digits).value;
+    }
+
+
     // 注: 不同浏览器toFixed有差异, chrome使用的是银行家舍入规则
     // 银行家舍入: 所谓银行家舍入法, 其实质是一种四舍六入五取偶(又称四舍六入五留双)法
     // 简单来说就是: 四舍六入五考虑, 五后非零就进一, 五后为零看奇偶, 五前为奇应舍去, 五前为偶要进一
     // 此处统一处理为四舍五入
-    Object.defineProperty(Number.prototype, 'toFixed2', {
+    Number.prototype.toFixed2 = (1.115).toFixed(2) !== '1.11' ? (0).toFixed : function (digits) {
 
-        enumerable: false,
-
-        value: (1.115).toFixed(2) !== '1.11' ? (0).toFixed : function (digits) {
-
-            return new Decimal(this).toFixed(digits);
-        }
-    });
+        return new Decimal(this).toFixed(digits);
+    };
 
 
 
