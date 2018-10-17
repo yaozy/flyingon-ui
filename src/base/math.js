f65fd481052edfa93087a0f6a2a288600f6df79b
yaozy <yaozy@126.com>
2018-10-17 23:13:55 +0800

    update

--- src/base/math.js
+++ src/base/math.js
@@ -1,58 +1,60 @@
 ;(function (Math) {
 
 
+
     var pow = Math.pow;
 
     var round = Math.round;
 
+    var toFixed = (0).toFixed;
+
+    var cache = new Decimal(0);
 
 
-    for (var i = 0; i < 50; i++)
+
+    for (var i = 0; i < 100; i++)
     {
-        pow[i] = pow(10, i);
+        pow[i] = Math.pow(10, i);
     }
 
-
-
-    function parse(value) {
-
-        var k;
-
-        value = ('' + value).split('.');
-
-        if (k = value[1])
-        {
-            value[0] = +(value[0] + k);
-            value[1] = pow[k = k.length] || (pow[k] = pow(10, k));
-        }
-        else
-        {
-            value[0] = +value[0];
-            value[1] = 1;
-        }
-
-        return value;
-    }
 
 
 
     // 小数处理类
     function Decimal(value) {
 
-        value = +value;
-        
-        if (value !== value || value === (value | 0))
-        {
-            this.v = value;
-            this.s = 1;
-        }
-        else
-        {
-            value = parse(value);
+        var v, e;
 
-            this.v = value[0];
-            this.s = value[1];
+        if (value)
+        {
+            if (value instanceof Decimal)
+            {
+                e = value.e;
+                v = value.v;
+            }
+            else if ((value = +value) === value)
+            {
+                if (value === (value | 0))
+                {
+                    v = value || 0;
+                }
+                else if (e = (v = ('' + value).split('.'))[1])
+                {
+                    e = e.length;
+                    v = +(v[0] + v[1]);
+                }
+                else
+                {
+                    e = 0;
+                    v = value;
+                }
+            }
         }
+
+        this.v = v || 0;
+        this.e = e || 0;
+
+        return this;
     }
     
 
@@ -60,6 +62,17 @@
 
     var prototype = (window.Decimal = Decimal).prototype;
 
+
+
+    prototype.clone = function () {
+
+        var result = Object.create(prototype);
+
+        result.v = this.v;
+        result.e = this.e;
+
+        return result;
+    }
 
 
     prototype.reset = function (value) {
@@ -71,40 +84,42 @@
 
     prototype.plus = function (value) {
 
-        var scale1, scale2;
-        
-        value = +value;
+        var e1, e2;
 
-        if (value !== value)
+        if (!value)
         {
-            this.v = value;
-            this.s = 1;
-
             return this;
         }
-
-        scale1 = this.s;
-
-        if (value === (value | 0))
+        
+        if (value instanceof Decimal)
         {
-            scale2 = 1;
+            e2 = value.e;
+            value = value.v;
+        }
+        else if ((value = +value) !== value)
+        {
+            return this;
+        }
+        else if (value === (value | 0))
+        {
+            e2 = 0;
         }
         else
         {
-            value = parse(value);
+            value = Decimal.call(cache, value);
 
-            scale2 = value[1];
-            value = value[0];
+            e2 = value.e;
+            value = value.v;
         }
         
-        if (scale1 > scale2)
+        if ((e1 = this.e) > e2)
         {
-            this.v += value * scale1 / scale2;
+            this.v += value * pow[e1] / pow[e2];
         }
-        else if (scale1 < scale2)
+        else if (e1 < e2)
         {
-            this.s = scale2;
-            this.v = this.v * scale2 / scale1 + value;
+            this.e = e2;
+            this.v = this.v * pow[e2] / pow[e1] + value;
         }
         else
         {
@@ -123,32 +138,51 @@
 
     prototype.mul = function (value) {
 
-        var scale;
+        var e;
 
-        value = +value;
-
-        if (value !== value)
+        if (!value)
         {
-            this.v = value;
-            this.s = 1;
-
+            this.v = this.e = 0;
             return this;
         }
-        
-        if (value === (value | 0))
+
+        if (!this.v)
         {
-            scale = 1;
+            return this;
+        }
+
+        if (value instanceof Decimal)
+        {
+            e = value.e;
+            value = value.v;
+        }
+        else if ((value = +value) !== value)
+        {
+            return this;
+        }
+        else if (value === (value | 0))
+        {
+            e = 0;
         }
         else
         {
-            value = parse(value);
+            value = Decimal.call(cache, value);
 
-            scale = value[1];
-            value = value[0];
+            e = value.e;
+            value = value.v;
         }
 
-        this.v *= value;
-        this.s *= scale;
+        if (value)
+        {
+            this.v *= value;
+        }
+        else
+        {
+            this.v = this.e = 0;
+            return this;
+        }
+
+        this.e += e;
 
         return this;
     }
@@ -156,41 +190,56 @@
 
     prototype.div = function (value) {
 
-        var scale1, scale2;
+        var e1, e2;
         
-        value = +value;
-
-        if (value !== value)
+        if (!value)
         {
-            this.v = value;
-            this.s = 1;
-
+            this.v = this.e = 0;
             return this;
         }
 
-        scale1 = this.s;
-        
-        if (value === (value | 0))
+        if (!this.v)
         {
-            scale2 = 1;
+            return this;
+        }
+
+        if (value instanceof Decimal)
+        {
+            e2 = value.e;
+            value = this.v / value.v;
+        }
+        else if ((value = +value) !== value)
+        {
+            this.v = this.e = 0;
+            return this;
+        }
+        else if (value === (value | 0))
+        {
+            e2 = 0;
         }
         else
         {
-            value = parse(value);
+            value = Decimal.call(cache, value);
 
-            scale2 = value[1];
-            value = value[0];
+            e2 = value.e;
+            value = value.v;
         }
 
-        if (scale1 !== scale2)
+        if (!value)
         {
-            if (scale1 > scale2)
+            this.v = this.e = 0;
+            return this;
+        }
+
+        if ((e1 = this.e) !== e2)
+        {
+            if (e1 > e2)
             {
-                value = this.v * scale2 / (value * scale1);
+                value = this.v / (value * pow[e1 - e2]);
             }
             else
             {
-                value = this.v * scale1 / (value * scale2);
+                value = this.v * pow[e2 - e1] / value;
             }
         }
         else
@@ -198,11 +247,11 @@
             value = this.v / value;
         }
 
-        value = parse(value);
+        value = Decimal.call(cache, value);
 
-        this.v = value[0];
-        this.s = value[1];
-        
+        this.v = value.v;
+        this.e = value.e;
+
         return this;
     }
 
@@ -211,13 +260,20 @@
 
         if (value |= 0)
         {
+            var v = this.v;
+
+            if (!v)
+            {
+                return this;
+            }
+
             if (value > 0)
             {
                 this.v *= pow[value];
             }
             else
             {
-                this.s += value;
+                this.e -= value;
             }
         }
 
@@ -227,17 +283,12 @@
 
     prototype.round = function (digits) {
 
-        var scale = this.s;
+        var e = this.e;
 
-        if (scale > 1 && (digits |= 0) > 0)
+        if ((digits |= 0) < e)
         {
-            digits = pow[digits];
-
-            if (scale > digits)
-            {
-                this.v = round(this.v * digits / scale);
-                this.s = digits;
-            }
+            this.v = round(this.v * pow[digits] / pow[e]);
+            this.e = digits;
         }
 
         return this;
@@ -246,40 +297,38 @@
 
     prototype.toFixed = function (digits) {
 
-        var scale1 = this.s;
+        var e = this.e;
 
         if ((digits |= 0) > 0)
         {
-            if (scale1 > 1)
+            if (e)
             {
-                var scale2 = pow[digits];
-
-                if (scale1 > scale2)
+                if (e > digits)
                 {
-                    return (round(this.v * scale2 / scale1) / scale2).toFixed(digits);
+                    return toFixed.call(round(this.v * pow[digits] / pow[e]) / pow[digits], digits);
                 }
 
-                return (this.v / scale1).toFixed(digits);
+                return toFixed.call(this.v / pow[e], digits);
             }
 
-            return this.v.toFixed(digits);
+            return toFixed.call(this.v, digits);
         }
         
-        return scale1 > 1 ? '' + round(this.v / scale1) : '' + this.v;
+        return e ? '' + round(this.v / pow[e]) : '' + this.v;
     }
 
 
     prototype.valueOf = function () {
         
-        var scale = this.s;
-        return scale > 1 ? this.v / scale : this.v;
+        var e = this.e;
+        return e ? this.v / pow[e] : this.v;
     }
 
 
     prototype.toString = function (k) {
 
-        var scale = this.s;
-        return (scale > 1 ? this.v / scale : this.v).toString(k);
+        var e = this.e;
+        return (e ? this.v / pow[e] : this.v).toString(k);
     }
 
 
@@ -289,17 +338,35 @@
 
         get: function () {
 
-            var scale = this.s;
-            return scale > 1 ? this.v / scale : this.v;
+            var e = this.e;
+            return e ? this.v / pow[e] : this.v;
         }
     });
 
 
 
-    // 可以指定小数位的四舍五入算法
-    Number.prototype.round2 = function (digits) {
+    // 重载四舍五入方法增加指定小数位数
+    Math.round = function (value, digits) {
 
-        return new Decimal(this).round(digits).value;
+        if ((digits |= 0) > 0)
+        {
+            if ((value = +value) !== value)
+            {
+                return value;
+            }
+
+            var items = ('' + value).split('.'),
+                decimal = items[1];
+
+            if (decimal.length <= digits)
+            {
+                return value;
+            }
+
+            return round(items[0] + decimal.slice(0, digits) + '.' + decimal[digits]);
+        }
+        
+        return round(value);
     }
 
 
@@ -307,10 +374,20 @@
     // 银行家舍入: 所谓银行家舍入法, 其实质是一种四舍六入五取偶(又称四舍六入五留双)法
     // 简单来说就是: 四舍六入五考虑, 五后非零就进一, 五后为零看奇偶, 五前为奇应舍去, 五前为偶要进一
     // 此处统一处理为四舍五入
-    Number.prototype.toFixed2 = (1.115).toFixed(2) !== '1.11' ? (0).toFixed : function (digits) {
+    if ((1.115).toFixed(2) === '1.11')
+    {
+        Number.prototype.toFixed = function (digits) {
 
-        return new Decimal(this).toFixed(digits);
-    };
+            return new Decimal(this).toFixed(digits);
+        }
+    }
+
+
+    // test
+    // new Decimal(.1).plus(.2).value === 0.3;
+    // new Decimal(10).mul(12.1).value === 121;
+    // new Decimal(2.135).round(2).value === '2.14';
+
 
 
 
